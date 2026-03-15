b361e6213783e2ce9edfe1bc115168f7c38c7ec4
Frnc36 <magoriferenc19@gmail.com>
2026-03-15 13:02:06 +0100

    Flex elrendezes

--- kartya.js
+++ kartya.js
@@ -1,12 +1,15 @@
-export function megjelenit(articleEELEM, adat = (fajnev, tipus, atlageletkor, erdekesseg, kep)){
-    let szoveg = `<div class="kartya">
-                <img class="card-img-top" src="${adat.kep}" alt="Card image">
-                <div class="card-body">
-                    <h4 class="card-title">${adat.fajnev}</h4>
-                    <p class="card-text">${adat.tipus}</p>
-                    <p class="card-text">${adat.atlageletkor}</p>
-                    <p class="card-text">${adat.erdekesseg}</p>
-                </div>
-            </div>`;
-    articleEELEM.innerHTML += szoveg;
-};
+export function megjelenit(
+  articleEELEM,
+  adat = (fajnev, tipus, atlageletkor, erdekesseg, kep),
+) {
+  let szoveg = `<div class="kartya">
+                        <div class="kartya-keret">
+                            <h4 class="kartya-fajnev">${adat.fajnev}</h4>
+                            <img class="kartya-kep" src="${adat.kep}" alt="Kartya kep">
+                            <p class="kartya-tipus">${adat.tipus}</p>
+                            <p class="kartya-eletkor">Átlagos életkor: ${adat.atlageletkor}</p>
+                            <p class="kartya-erdekesseg">${adat.erdekesseg}</p>
+                        </div>
+                </div>`;
+  articleEELEM.innerHTML += szoveg;
+}
